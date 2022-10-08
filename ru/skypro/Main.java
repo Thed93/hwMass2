bfc9c29b139b48cc73cecb8ece16b6a10f6b2e34
Evgeniy Zinchenko <JjjackVat@gmail.com>
2022-10-08 09:26:20 +0300

    создание

--- ru/skypro/Main.java
+++ ru/skypro/Main.java
@@ -11,17 +11,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Tusk 1" + "\n");
-        int[] dailySpending = generateRandomArray();
-        int totalSpending = 0;
-        for (int j : dailySpending) {
-            totalSpending = totalSpending + j;
-        }
-        System.out.println("Сумма трат за месяц составила " + totalSpending + " рублей.");
-    }
-
-
-
-
 
     }
+}
